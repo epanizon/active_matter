ff0b5b737102565958743900e567b89398d16c5a
Emanuele Panizon <epanizon@gmail.com>
2017-11-08 15:48:32 +0100

    Let's try and use classes

--- active.cpp
+++ active.cpp
@@ -9,32 +9,49 @@
 using namespace std;
 
 class Particle{
-  double pos[2];
-  double theta;
+  double *pos;
+  double *vel;
+  double theta {0.0};
 public:
+  Particle():{
+    pos = new double[2]{0.0,0.0};
+    vel = new double[2]{0.0,0.0};
+  }
+  ~Particle():{
+    delete[] pos;
+    delete[] vel;
+    pos = nullptr;
+    vel = nullptr;
+  }
   void put_pos(double x, double y){
     pos[0] = x;
     pos[1] = y;
   }
-  void put_theta(double dir){
-    theta = dir;
+  void put_vel(double x, double y){
+    vel[0] = x;
+    vel[1] = y;
   }
-  double* get_pos(){
-    return pos;
-  }
-  double get_theta(){
-    return theta;
-  }
+  void put_theta(double dir){theta = dir;}
+  // shitty, but for now...
+  ostream& print_pos(){
+    ostream& os;
+    os << pos[0] << " " << pos[1];
+  } 
+  void print_vel(){cout << vel[0] << " " << vel[1] << "\n";}
+  void print_theta(){cout << theta << "\n";}
+
+  
+
 };
 
 
-  int main(int argc, char** argv){
+int main(int argc, char** argv){
     if (argc){cout << "argc is "<< argc <<"\n";}
     Particle P1;
     P1.put_pos(1.1,3.2);
     P1.put_theta(0.2);
     
-    cout << "pos is " << P1.get_pos() << "\n";
-    cout << "dir is " << P1.get_theta() << "\n";
+    // mah
+    cout << "pos is " << print_pos() << "\n";
     return 0;
-  }
+}
