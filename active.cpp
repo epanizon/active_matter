723493aabc273b5ca37b7d559981e54b189d212b
Emanuele Panizon <epanizon@gmail.com>
2017-11-08 11:55:48 +0100

    Removed Random.* (not c++11). Modified active.cpp

--- active.cpp
+++ active.cpp
@@ -1,40 +1,40 @@
-#include "Random.h"
 #include <string>
 #include <cstring>
 #include <cstdio>
 #include <cmath>
 #include <vector>
 #include <cstdlib>
+#include <iostream>
 
 using namespace std;
 
 class Particle{
-  vector<double> pos[2];
+  double pos[2];
   double theta;
 public:
   void put_pos(double x, double y){
-    pos[1] = x;
-    pos[2] = y;
+    pos[0] = x;
+    pos[1] = y;
   }
-  void put_dir(double dir){
+  void put_theta(double dir){
     theta = dir;
   }
-  vector<double> get_pos(){
+  double* get_pos(){
     return pos;
   }
-  vector<double> get_dir(){
-    return dir;
+  double get_theta(){
+    return theta;
   }
-}
+};
 
 
   int main(int argc, char** argv){
     if (argc){cout << "argc is "<< argc <<"\n";}
     Particle P1;
     P1.put_pos(1.1,3.2);
-    P1.put_dir(0.2);
+    P1.put_theta(0.2);
     
     cout << "pos is " << P1.get_pos() << "\n";
-    cout << "dir is " << P1.get_dir() << "\n";
+    cout << "dir is " << P1.get_theta() << "\n";
     return 0;
   }
