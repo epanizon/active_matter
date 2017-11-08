fc840ac8dea619576f900b8717d70539b98ab536
Emanuele Panizon <epanizon@gmail.com>
2017-11-08 11:44:22 +0100

    Test for class Particle

--- active.cpp
+++ active.cpp
@@ -1,4 +1,3 @@
-#include "Vector.h"
 #include "Random.h"
 #include <string>
 #include <cstring>
@@ -8,3 +7,34 @@
 #include <cstdlib>
 
 using namespace std;
+
+class Particle{
+  vector<double> pos[2];
+  double theta;
+public:
+  void put_pos(double x, double y){
+    pos[1] = x;
+    pos[2] = y;
+  }
+  void put_dir(double dir){
+    theta = dir;
+  }
+  vector<double> get_pos(){
+    return pos;
+  }
+  vector<double> get_dir(){
+    return dir;
+  }
+}
+
+
+  int main(int argc, char** argv){
+    if (argc){cout << "argc is "<< argc <<"\n";}
+    Particle P1;
+    P1.put_pos(1.1,3.2);
+    P1.put_dir(0.2);
+    
+    cout << "pos is " << P1.get_pos() << "\n";
+    cout << "dir is " << P1.get_dir() << "\n";
+    return 0;
+  }
